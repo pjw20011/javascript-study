248cfebd617ac789c62e5cd648e6cc9194d49f41
Jeongwon Park <pjw200116@gmail.com>
2025-02-07 17:35:02 +0900

    var,let,const

--- var,let,const/script.js
+++ var,let,const/script.js
@@ -1,2 +1,33 @@
-var A =1;
-console.log(A);
+//중복 선언 가능, 재할당 가능, 함수레벨 스코프
+
+function func(){
+    if (true) {
+        var a = 1;
+        console.log(a);
+    }
+}
+// console.log(a); // 오류
+
+
+
+// 중복선언 불가, 재할당 가능
+let greeting = 'hello';
+console.log(greeting);
+
+greeting = 'Bye';
+console.log(greeting);
+
+//중복선언 불가, 재할당 불가
+const mention = 'Hi';
+
+// let, const block 레벨 스코프
+
+function func2(){
+    if (true) {
+        let w = 'w';
+        console.log(w);
+    }
+ // 오류   console.log(w);
+}
+
+func2();
